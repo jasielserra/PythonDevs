525e580e3e7c0b9bd04a53c38283a4e1d2cd741e
Jasiel Serra <jasiel_serra@hotmail.com>
2017-09-07 19:10:16 -0300

    Using 3D cube

--- cudo_3D_grid.py
+++ cudo_3D_grid.py
@@ -1,4 +1,5 @@
 x = int ( raw_input())
 y = int ( raw_input())
+z = int ( raw_input())
 n = int ( raw_input())
-print [ [i,j] for i in range(x+1) for j in range(y+1) if i+j != n ]
+print[[ i , j, x] for i in range ( x + 1 ) for j in range( y + 1) for x in range( z + 1) if ((i+j+x) != n)]
