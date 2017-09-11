7c5cba73f3753e28f92e5d875714fe3d66257d57
Jasiel Serra <jasiel_serra@hotmail.com>
2017-09-11 00:52:48 -0300

    Working with nested list.

---
+++ Nested_list.py
@@ -0,0 +1,32 @@
+'''
+Given the names and grades for each student in a Physics class of N students,
+store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+Note: If there are multiple students with the same grade, order their names alphabetically and print
+each name on a new line.
+
+By HackerRank
+
+'''
+
+
+students = []
+
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    students.append([name,score])
+        
+students.sort(key=lambda x: x[1])
+second_highest = students[0][1]
+    
+for name,grade in students:
+    if grade > second_highest:
+        second_highest = grade
+            break
+
+names = []     
+
+for name, grade in sorted(students):
+    if grade == second_highest:
+            names.append(name)
